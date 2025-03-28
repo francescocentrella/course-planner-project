4b83636dd1f40d2c751fe45dde6e9a09e4ae6d00
Francesco <francesco.pio.centrella@gmail.com>
2025-03-28 15:13:45 -0700

    updated requirements

--- backend/requirements.py
+++ backend/requirements.py
@@ -1,27 +1,54 @@
-bible30 = {"biblical interpretation and spirtitual formation": "BBST103",
-         "foundation of christian thought": "BBST165",
-         "old testament history and literature": "BBST209",
-         "new testament history and literature": "BBST210",
-         "theology I": "BBST251",
-         "theology II": "BBST354",
-         "gospel, kingdom, and culture": "BBST365",
-         "BBST 300/400 bible/theology elective": "BBST300/400",
-         "integration seminar":"BBST465"}
+class CS_datascience:        
+    general_ed = {"first year seminar": "GNST102",
+                "behavioral science": ["ANTH200", "ANTH310", "PSYC200", "SOCI220", "SOCI262"],
+                "communication": ["COMM200", "COMM235", "COMM280", "COMM281"],
+                "studies in critical thinking & writing": ["ENGL100", "ENGL112"],
+                "fine arts": ["ARTS100", "ARTS107", "ARTS108", "ARTS110", "ARTS111", "ARTS112", "ARTS210", "ARTS211", "ARTS213", "ARTS221"
+                                "CNMA110", "THTR160",
+                                "MUSC101", "MUSC115", "MUSC314"],
+                "history": ["HIST100", "HIST101", "HIST200", "HIST201", "POSC225"],
+                "kinesiology": "KEN107",
+                "kinesiology (activity)": ["KEN101", "KEN110", "KEN130", "KEN140", "KEN150"],
+                "literature": ["ENGL220", "ENGL230", "ENGL283", "ENGL290"],
+                "philosophy": ["PHIL210", "PHIL211", "PHIL212", "PHIL213", "PHIL214", "PHIL215", "PHIL216", "PHIL217"
+                               "PHIL218", "PHIL240", "PHIL340"],
+                "science": ["ANTH222", "BIOS100", "BIOS103", "BIOS120", "BIOS121", 
+                            "CHEM100", "CHEM105", "CHEM107", "CHEM120", 
+                            "PHSC101", "PHSC102", "PHSC103", "PHSC104", "PHSC105", "PHSC109", "PHSC110", "PHSC250"]}
+    
+    electives = {"introduction to computer science": "CSCI105",
+                 "data structures": "CSCI106",
+                 "computer organization": "CSCI220",
+                 "programming languages": "CSCI230",
+                 "computer communications": "CSCI430",
+                 "software enineering": "CSCI450", 
+                 "management info systems": "BUSN220",
+                 "business analytics": "BUSN323",
+                 "database management": "CSCI402",
+                 "calculus": ["MATH140", "MATH150"],
+                 "upper-division 1": ["CSCI305", "CSCI306", "CSCI311", "CSCI335"],
+                 "upper-division 2": ["CSCI305", "CSCI306", "CSCI311", "CSCI335", "CSCI400", "CSCI402", "CSCI420", "CSCI430", "CSCI440", "CSCI450"],
+                 "CSCI/BUSN/MATH electives": ["CSCI104", "CSCI400", "CSCI440", "CSCI480",
+                                              "BUSN211", "BUSN212", "BUSN370", "BUSN423",
+                                              "MATH151", "MATH291", "MATH319", "MATH321", "MATH331", "MATH332", "MATH333", "MATH380", "MATH470"]}
+    
+    bible30 = {"biblical interpretation and spirtitual formation": "BBST103",
+            "foundation of christian thought": "BBST165",
+            "old testament history and literature": "BBST209",
+            "new testament history and literature": "BBST210",
+            "theology I": "BBST251",
+            "theology II": "BBST354",
+            "gospel, kingdom, and culture": "BBST365",
+            "BBST 300/400 bible/theology elective": "BBST300/400",
+            "integration seminar":"BBST465"}
 
 
-bible18 = {"biblical interpretation and spirtitual formation": "BBST103",
-         "foundation of christian thought": "BBST165",
-         "old testament history and literature": "BBST209",
-         "new testament history and literature": "BBST210",
-         "christian theology": "BBST260",
-         "gospel, kingdom, and culture": "BBST365"}
+    bible18 = {"biblical interpretation and spirtitual formation": "BBST103",
+            "foundation of christian thought": "BBST165",
+            "old testament history and literature": "BBST209",
+            "new testament history and literature": "BBST210",
+            "christian theology": "BBST260",
+            "gospel, kingdom, and culture": "BBST365"}
+    
 
-
-general_ed = {"first year seminar": "GNST102",
-              "behavioral science": ["ANTH200", "ANTH310", "PSYC200", "SOCI220", "SOCI262"],
-              "communication": ["COMM200", "COMM235", "COMM280", "COMM281"],
-              "studies in critical thinking & writing": ["ENGL100", "ENGL112"],
-              "fine arts": ["ARTS100", "ARTS107", "ARTS108", "ARTS110", "ARTS111", "ARTS112", "ARTS210", "ARTS211", "ARTS213", "ARTS221"
-                            "CNMA110", "THTR160",
-                            "MUSC101", "MUSC115", "MUSC314"]}
-
+    

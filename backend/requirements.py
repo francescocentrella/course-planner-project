f26c396d08188bb136cd5e2ed3801ece4bbb5c4b
Francesco <francesco.pio.centrella@gmail.com>
2025-03-28 14:09:59 -0700

    requirements start

---
+++ backend/requirements.py
@@ -0,0 +1,27 @@
+bible30 = {"biblical interpretation and spirtitual formation": "BBST103",
+         "foundation of christian thought": "BBST165",
+         "old testament history and literature": "BBST209",
+         "new testament history and literature": "BBST210",
+         "theology I": "BBST251",
+         "theology II": "BBST354",
+         "gospel, kingdom, and culture": "BBST365",
+         "BBST 300/400 bible/theology elective": "BBST300/400",
+         "integration seminar":"BBST465"}
+
+
+bible18 = {"biblical interpretation and spirtitual formation": "BBST103",
+         "foundation of christian thought": "BBST165",
+         "old testament history and literature": "BBST209",
+         "new testament history and literature": "BBST210",
+         "christian theology": "BBST260",
+         "gospel, kingdom, and culture": "BBST365"}
+
+
+general_ed = {"first year seminar": "GNST102",
+              "behavioral science": ["ANTH200", "ANTH310", "PSYC200", "SOCI220", "SOCI262"],
+              "communication": ["COMM200", "COMM235", "COMM280", "COMM281"],
+              "studies in critical thinking & writing": ["ENGL100", "ENGL112"],
+              "fine arts": ["ARTS100", "ARTS107", "ARTS108", "ARTS110", "ARTS111", "ARTS112", "ARTS210", "ARTS211", "ARTS213", "ARTS221"
+                            "CNMA110", "THTR160",
+                            "MUSC101", "MUSC115", "MUSC314"]}
+
